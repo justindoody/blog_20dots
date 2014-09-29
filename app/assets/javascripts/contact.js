c022f3413b49ac9213c0a6ad4ceb01832ae71c9a
jdprgm <stickmedia@gmail.com>
2014-09-29 17:24:14 -0400

    Added a background blur effect for the contact details

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -3,11 +3,13 @@ $(document).on("page:change", contact);
 function contact() {
   $('#modal_mask').click(function () {
     $(this).fadeOut(200);
+    $('#sidebar, #all_posts, #content, #header_wrapper').removeClass('blurred');
     $('.modal').removeClass('bounceIn').addClass('animated bounceOut');
   });
 
   $('#contact_button').click(function(e) {
     e.preventDefault();
+    $('#sidebar, #all_posts, #content, #header_wrapper').addClass('blurred');
     $('#modal_mask').fadeIn(200);
     $('.modal').removeClass('bounceOut').delay(200).show().addClass('animated bounceIn');
   });
