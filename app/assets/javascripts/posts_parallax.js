323c9793a92878f078e73890476e39ac4e3568ba
jdprgm <stickmedia@gmail.com>
2014-11-18 17:06:03 -0500

    CSS Blur on Post pages fully working

--- app/assets/javascripts/posts_parallax.js
+++ app/assets/javascripts/posts_parallax.js
@@ -6,7 +6,7 @@ function parallax() {
 	$(window).unbind();
 	if ($('body').data("controller") == "parallax") // Checks if we are on the right page
 	{
-		var header_height = $('#header_container_blurred').height();
+		var header_height = $('#header_wrapper').height();
 		$("#sidebar").data("top", $("#sidebar").offset().top); // set original position on load
 		
 		// Do the following on scroll...
@@ -14,29 +14,25 @@ function parallax() {
 			var scrolled = $(window).scrollTop(); // Both parallax and fader need this, might as well set here.
 			parallaxScroll(scrolled);
 
-			$('#header_container_blurred').css('opacity', ((scrolled / header_height) * 2.5)); // Fader, no need for separate function
-			sidebar_location(); // Freezes the sidebar once scrolled past the header
+			$('#crisp').css('opacity', (1 - (scrolled / header_height) * 2.5)); // Fader, no need for separate function
+			sidebar_location(scrolled); // Freezes the sidebar once scrolled past the header
 		});
 		
 		var post_height = $('#post_height').height(); // Height of the div containing the Date and Post Title, not changing might as well hard set
 
 		
 		function parallaxScroll(scrolledY) {		
-			$('#header_container, #header_container_blurred, #header_container_text').css('top', '-' + ((scrolledY * 0.1)) + 'px');
-			var visible_container_height = ($('#header_wrapper').height() + $('#header_container').position().top) - scrolledY;
-			console.log(visible_container_height);
+			var visible_container_height = ($('#header_wrapper').height() - scrolledY);
+			
 			// Prevents post title paralax effect from going outside of the parent div
 			if (visible_container_height >= post_height) {
-				if (post_height > 120) {
-					$('.post_title').css('top', (scrolledY * 0.75) + 'px');
-				} else {
-					$('.post_title').css('top', (scrolledY * 0.99) + 'px');
-				}
+				$('#header_container_text').css('height', (visible_container_height) + 'px');
 			}
 		}
 
-		function sidebar_location() {
-			if ($(window).scrollTop() > $("#sidebar").data("top") && $(window).width() > 970) { // Freeze sidebar once scrolled past header
+		function sidebar_location(scrolledY) {
+			console.log($("#sidebar").data("top") - scrolledY);
+			if (scrolledY > $("#sidebar").data("top") && $(window).width() > 970) { // Freeze sidebar once scrolled past header
 				$('#sidebar').css({
 					'position'	: 'fixed',
 					'top'		: '0',
@@ -53,5 +49,17 @@ function parallax() {
 				});
 			}
 		}	
+
+		$( window ).resize(function() {
+			var ratio = $('#header_wrapper').width()/$('#header_wrapper').height();
+			
+			if (ratio > 4) {
+				$('.bg').removeClass('height');
+				$('.bg').addClass('width');
+			} else if (ratio < 4) {
+				$('.bg').removeClass('width');
+				$('.bg').addClass('height');
+			}
+		});
 	}
 }
