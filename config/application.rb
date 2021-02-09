1eebf1553b3d5595e1d6f55d469a49ce26a726e5
justindoody <stickmedia@gmail.com>
2021-02-09 13:30:39 -0500

    mostly environment cleanup

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+ENVied.require(*Rails.groups)
 
 module Blog20dots
   class Application < Rails::Application
