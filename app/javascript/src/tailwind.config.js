abc67b5859fc5cea4e2e6c02e35e36c1440c3dd5
justindoody <stickmedia@gmail.com>
2021-02-13 15:10:58 -0500

    converting to Tailwind CSS

---
+++ app/javascript/src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'sidebar': ['"Avenir Next"', 'ui-sans-serif', 'system-ui'],
+            'serif': ['ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif']
+        }
+    },
+    variants: {},
+    plugins: [],
+  }
